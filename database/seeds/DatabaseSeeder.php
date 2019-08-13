aeb3d4a240248f6d4390275234219014bb26bdf9
kevchike <kevchike@gmail.com>
2019-08-13 21:54:37 +0100

    Run seeder on heroku

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserTableSeeder::class);
-        // $this->call(BookTableSeeder::class);
-        // $this->call(ReviewTableSeeder::class);
+        $this->call(UserTableSeeder::class);
+        $this->call(BookTableSeeder::class);
+        $this->call(ReviewTableSeeder::class);
     }
 }
