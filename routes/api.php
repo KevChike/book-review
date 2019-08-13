3abb9c70a7c4e297b9c7339b6d37b910f8530467
kevchike <kevchike@gmail.com>
2019-08-13 01:14:38 +0100

    Refactor routes

--- routes/api.php
+++ routes/api.php
@@ -6,17 +6,23 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['prefix' => '/v1/auth', 'namespace' => 'Auth'], function () {
-	Route::post('/login', 'LoginController@apiLogin')->name('api.login');
-	Route::get('/logout', 'LoginController@apiLogout')->name('api.logout')->middleware('auth:api');
-	Route::get('/user', 'LoginController@loggedInUser')->name('api.loggedin.user')->middleware('auth:api');
+Route::group(['prefix' => '/v1', 'namespace' => 'Auth'], function () {
+	Route::group(['prefix' => '/auth'], function () {
+		Route::post('/login', 'LoginController@apiLogin')->name('api.login');
+		Route::get('/logout', 'LoginController@apiLogout')->name('api.logout')->middleware('auth:api');
+		Route::get('/user', 'LoginController@loggedInUser')->name('api.loggedin.user')->middleware('auth:api');
+	});
 });
 
 Route::group(['prefix' => '/v1', 'namespace' => 'Api'], function () {
-	Route::get('/books', 'BookController@index')->name('api.books.index');
-	Route::post('/books', 'BookController@store')->name('api.books.store');
-	Route::put('/books/{id}', 'BookController@update')->name('api.books.update');
-	Route::delete('/books/{id}', 'BookController@destroy')->name('api.books.destroy');
+	Route::group(['prefix' => '/books'], function () {
+		Route::get('/', 'BookController@index')->name('api.books.index');
+		Route::post('/', 'BookController@store')->name('api.books.store');
+		Route::put('/{id}', 'BookController@update')->name('api.books.update');
+		Route::delete('/{id}', 'BookController@destroy')->name('api.books.destroy');
+	});
 
-	Route::post('/reviews', 'ReviewController@store')->name('api.reviews.store');
+	Route::group(['prefix' => '/reviews'], function () {
+		Route::post('/', 'ReviewController@store')->name('api.reviews.store');
+	});
 });
