600c4157461dc31e6ad298c6fe1af17d1f108c2e
kevchike <kevchike@gmail.com>
2019-08-15 01:06:55 +0100

    Remove unnecessary routes

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,7 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::fallback(function(){
+    return response()->json([
+        'message' => 'Not Found.'
+    ], 404);
 });
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
